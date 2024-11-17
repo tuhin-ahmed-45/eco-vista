10def4dd0da0079cafef1d0a425f1f766f1d8912
Tuhin Ahmed <mdtuhin.dev@gmail.com>
2024-11-17 22:10:42 +0600

    Weather Dashboard

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
-import LocationDetector from "@/components/Location/LocationDetector";
+import dynamic from 'next/dynamic';
+
+const LocationDetector = dynamic(() => import('@/components/Location/LocationDetector'), { ssr: false });
+
 
 export default function Home() {
   return (
--- components/Location/LocationDetector.jsx
+++ components/Location/LocationDetector.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 
 export default function LocationDetector() {
@@ -23,23 +23,25 @@ export default function LocationDetector() {
                 router.push(`/current?${params.toString()}`)
             })
         }
-    }, [pathname, searchParams])
+    }, [pathname, router, searchParams])
     return (
-        <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
-            {
-                loading && (
-                    <>
-                        <Image
-                            src="/network.gif"
-                            alt="Loading..."
-                            height={500}
-                            width={500}
-                            className="border rounded-md my-4"
-                        />
-                        <p className="font-loading">Detecting your current location...</p>
-                    </>
-                )
-            }
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
+                {
+                    loading && (
+                        <>
+                            <Image
+                                src="/network.gif"
+                                alt="Loading..."
+                                height={500}
+                                width={500}
+                                className="border rounded-md my-4"
+                            />
+                            <p className="font-loading">Detecting your current location...</p>
+                        </>
+                    )
+                }
+            </div>
+        </Suspense>
     );
 }
